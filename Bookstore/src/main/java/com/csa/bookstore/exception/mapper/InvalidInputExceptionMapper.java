0649820c9f354f60fc3f53a3ff65af5bb9190e6a
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-14 17:16:23 +0530

    Created InvalidInputException and implemented ExceptionMapper

---
+++ Bookstore/src/main/java/com/csa/bookstore/exception/mapper/InvalidInputExceptionMapper.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csa.bookstore.exception.mapper;
+
+import com.csa.bookstore.entity.ErrorResponse;
+import com.csa.bookstore.exception.InvalidInputException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author faaeq
+ */
+
+@Provider
+public class InvalidInputExceptionMapper implements ExceptionMapper<InvalidInputException> {
+    
+    @Override
+    public Response toResponse(InvalidInputException exception) {
+        ErrorResponse errorResponse = new ErrorResponse("Invalid Input", exception.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST)
+                      .entity(errorResponse)
+                      .type(MediaType.APPLICATION_JSON)
+                      .build();
+    }
+    
+}
