0253bdab52b9a6cfbea01bd4b1d5aaeaae4d65e2
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-14 17:04:52 +0530

    Created BookNotFoundException and implemented ExceptionMapper

---
+++ Bookstore/src/main/java/com/csa/bookstore/exception/BookNotFoundException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csa.bookstore.exception;
+
+/**
+ *
+ * @author faaeq
+ */
+
+public class BookNotFoundException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+    
+}
