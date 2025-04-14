aa08349f39642a1c74dfe1195ef89de11bfa5bcd
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-14 17:11:01 +0530

    Created CartNotFoundException and implemented ExceptionMapper

---
+++ Bookstore/src/main/java/com/csa/bookstore/exception/mapper/CartNotFoundExceptionMapper.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csa.bookstore.exception.mapper;
+
+import com.csa.bookstore.entity.ErrorResponse;
+import com.csa.bookstore.exception.CartNotFoundException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author faaeq
+ */
+
+@Provider
+public class CartNotFoundExceptionMapper implements ExceptionMapper<CartNotFoundException> {
+    
+    @Override
+    public Response toResponse(CartNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse("Cart Not Found", exception.getMessage());
+        return Response.status(Response.Status.NOT_FOUND)
+                      .entity(errorResponse)
+                      .type(MediaType.APPLICATION_JSON)
+                      .build();
+    }
+    
+}
