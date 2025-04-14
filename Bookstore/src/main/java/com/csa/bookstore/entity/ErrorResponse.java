ed25e4d3f76779a69ba5ca28ff1e3faddec67bac
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-14 16:58:06 +0530

    Create ErrorResponse.java

---
+++ Bookstore/src/main/java/com/csa/bookstore/entity/ErrorResponse.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csa.bookstore.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author faaeq
+ */
+
+@XmlRootElement
+public class ErrorResponse {
+    
+    private String error;
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
+
